57924922a6232108442712ed2296a6b785cd9264
cryptic20 <sherwin.wyco20@gmail.com>
2018-05-11 17:43:49 -0400

    edit name on spotify app

--- static/script.js
+++ static/script.js
@@ -35,7 +35,7 @@ if (!_token) {
 
 window.onSpotifyPlayerAPIReady = () => {
   const player = new Spotify.Player({
-    name: 'Web Playback SDK Template',
+    name: 'Discordify',
     getOAuthToken: cb => { cb(_token); }
   });
 
