35b2effad6d38793fe7833e4aee0ba22dc5453fa
cryptic20 <sherwin.wyco20@gmail.com>
2018-05-11 16:42:34 -0400

    fixed express server

--- Bot.js
+++ Bot.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log('listening to port 3000');
 });
 
